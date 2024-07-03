d4671e8fc46f61b9d19aee0027576a57cabc63a2
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-07-03 10:05:40 +0530

    Add files via upload

---
+++ Largest Number in List.py
@@ -0,0 +1,6 @@
+list = []
+num = int(input("enter elements in a list"))
+for i in range(1,num+1):
+      ele=int(input("enter elements:"))
+      list.append(ele)
+print("Largest element is:", max(list))
